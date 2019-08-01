c6119313fe3b984a7839ef83317d10eb3b5c8957
Huydzo <huydzo88@gmail.com>
2019-08-01 16:07:49 +0700

    - index 1

--- js/main.js
+++ js/main.js
@@ -33,30 +33,31 @@ overlayIconMobile = () => {
     });
 };
 
-//Scroll to Section
-scrollToSection = () => {
-    $(document).on("scroll", onScroll);
-        $('.js_scroll').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
+//Banner Home
+bannerHomeSlide = () => {
+  let bannerHomeSlide = $('#banner_home');
+    bannerHomeSlide.slick({
+        infinite: true,
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 2,
+                }
+            },
 
-        let target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-};
-
-onScroll = (event) => {
-    let scrollPos = $(document).scrollTop();
-    $('.item_service').each(function () {
-        let currLink = $(this);
-        let refElement = $(currLink.attr("href"));
+        ]
     });
 };
 
@@ -76,104 +77,6 @@ backTopTop = () => {
     });
 };
 
-//Fix Header
-/*fixHeader = () => {
-    let header = $('#header');
-    let offsetHeader = header.offset().top;
-    let heightWindow = $(window).scrollTop();
-    //console.log(offsetHeader);
-    if (heightWindow > offsetHeader) {
-        header.addClass('fixed');
-    }else  {
-        header.removeClass('fixed');
-    }
-    $(window).scroll(function () {
-        let heightWindow = $(window).scrollTop();
-        if (heightWindow > offsetHeader) {
-            header.addClass('fixed');
-        }else  {
-            header.removeClass('fixed');
-        }
-    })
-};*/
-
-//Slide Partner
-slidePartner = () => {
-    var swiper = new Swiper('.partner_slide', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-};
-
-//Highlight Slide
-slideHighLight = () => {
-    var swiper2 = new Swiper('.highlight_news_slide', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        breakpoints: {
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 30,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 15,
-            },
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-            }
-        }
-    });
-};
-
-suggestScroll = () => {
-    var swiper3 = new Swiper('.suggest_scroll', {
-        slidesPerView: 'auto',
-        //spaceBetween: 30,
-        freeMode: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-};
-
-categorySlide = () => {
-    var swiper4 = new Swiper('.category_slide', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-};
-
-queryOrder = () => {
-    let item = $('.order_query_option');
-    let content = $ ('.order_query_form');
-    item.click(function () {
-        item.removeClass('active');
-        $(this).addClass('active');
-        let index = $(this).index();
-        console.log(index);
-        content.removeClass('active');
-        content.eq(index).addClass('active');
-    });
-};
 openHeaderSearch = () => {
     let btnHeaderSearch = $('.header_search_button');
     let searchIcon = $('.header_search_button i');
@@ -190,11 +93,6 @@ openHeaderSearch = () => {
 $(document).ready(function () {
     //preloader ();
     overlayIconMobile ();
-    scrollToSection();
-    slidePartner();
-    slideHighLight();
-    suggestScroll();
-    categorySlide();
-    queryOrder();
+    bannerHomeSlide();
     openHeaderSearch();
 });
